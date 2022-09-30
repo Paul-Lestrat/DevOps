0cba4b5b071452b742e016ac9cbe1cc456221864
Paul-Lestrat <paul.l@datascientest.com>
2022-09-30 11:16:41 +0200

    test_fail

--- tests.py
+++ tests.py
@@ -3,4 +3,8 @@ from utils import simple_add
 
 def test_1():
     added = simple_add(3, 5)
-    assert added == 8
+    assert added == 9
+    
+def test_2():
+    added = simple_add(7, 9)
+    assert added == 16
