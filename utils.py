c6a3f67a4a68f42c32a99ead2472c48ce5a1b916
Paul-Lestrat <paul.l@datascientest.com>
2022-11-15 17:09:37 +0000

    deleted unnecessary code

--- utils.py
+++ utils.py
@@ -2,13 +2,4 @@ def simple_add(a, b):
     return a + b
 
 
-def simple_substract(a, b):
-    return a - b
 
-
-def simple_multiply(a, b):
-    return a * b
-
-
-def simple_divide(a, b):
-    return a / b
